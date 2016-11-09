7c04bd1cabd85caea8314b7392807cd46d76d34f
1jasonmurray <1jasonmurray@gmail.com>
2016-11-09 09:19:41 -0800

    Completed first part of run_analysis.R

--- cleaningdata/Week4/tidydataproj/run_analysis.R
+++ cleaningdata/Week4/tidydataproj/run_analysis.R
@@ -19,7 +19,6 @@ run_analysis <- function(dataDir = "."){
 	## Create working data directories if they don't exist
 	rawPath <- paste(dataDir, "/raw", sep = "")
 	tidyPath <- paste(dataDir,"/tidied", sep = "")
-	
 	if (!dir.exists(rawPath))  {dir.create(rawPath)}
 	if (!dir.exists(tidyPath)) {dir.create(tidyPath)}
 	
@@ -28,7 +27,6 @@ run_analysis <- function(dataDir = "."){
 	## Download and unzip the data set
 	dataURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 	dataFile <- paste(rawPath, "/dataset.zip", sep = "" )
-	
 	#download.file(dataURL,dataFile)
 	#unzip(dataFile, exdir = rawPath)
 	
@@ -41,7 +39,7 @@ run_analysis <- function(dataDir = "."){
 	featureFile <- paste(dataSetPath, "/features.txt", sep = "")
 	activityFile <- paste(dataSetPath, "/activity_labels.txt", sep = "")
 	
-	# Train Files
+	# Training Files
 	trainDataFile <- paste(dataSetPath, "/train", "/X_train.txt", sep = "")
 	trainLabelsFile <- paste(dataSetPath, "/train", "/y_train.txt", sep = "") 
 	trainSubjectFile <- paste(dataSetPath, "/train", "/subject_train.txt", sep = "")
@@ -53,19 +51,56 @@ run_analysis <- function(dataDir = "."){
 	
 	
 	
-	## Read in data from files to data frames		
-	features <- read.table(featureFile,col.names = c("index","feature"))
+	## Read in data from files to data frames, clean up feature name formating
+	
+	# Function to clean up feature formatting by removing "()" and replacing - and , with _
+	fix_format <- function(x) { 
+		y <- gsub("\\(|\\)","", x) 
+		z <- gsub("\\-", "_", y)
+		gsub(",","_",z)
+	}
+	
+	#Import and clean index files
+	features_import <- read.table(featureFile,col.names = c("index","feature"))
+	features <- sapply(features_import$feature,fix_format)
 	activities <- read.table(activityFile, col.names = c("index","activity"))
 	
 	# Training Data
-	trainData <- read.table(trainDataFile, col.names = features$feature)
+	trainData <- read.table(trainDataFile, col.names = features)
 	trainLabels <- read.table(trainLabelsFile, col.names = "label")
-	trainSubjects <- read.table(trainSubjectFile, col.names = "subject")
+	trainSubjects <- read.table(trainSubjectFile, col.names = "Subject")
 	
 	# Test Data
-	testData <- read.table(testDataFile, col.names = features$feature)
+	testData <- read.table(testDataFile, col.names = features)
 	testLabels <- read.table(testLabelsFile, col.names = "label")
-	testSubjects <- read.table(testSubjectFile, col.names = "subject")
+	testSubjects <- read.table(testSubjectFile, col.names = "Subject")
 	
+	
+	
+	## Merge test and training data sets
+	Data <- rbind(trainData, testData)
+	Labelsn <- rbind(trainLabels, testLabels)
+	Subjects <- rbind(trainSubjects, testSubjects)
+	
+	
+	
+	## Replace numeric labels with desciprtive names from activities index
+	Labels <- sapply(Labelsn$label, function(index) Labelsn$label <- activities[index, 2])
+
+	
+	
+	##Merge all data into one frame with with cbind in the following order: Subjects, Labels, Data
+	allData <- cbind(Subjects, Labels, Data)
+
+	
+	
+	## Filter out only the mean and standard deviation data along with Subjects/Labels column
+	filteredData <- select(allData,matches("Subject|Labels|.*mean.*|.*std.*"))
+
+	
+	
+	## Write data to file all nice and formatted to work with later
+	outfile <- paste(tidyPath, "/tidyUCRdata", sep = "")
+	write.table(filteredData, file = outfile)
 	
 }
